8ea885d7b34ea59a7e7ce02932abca103c20735e
ss100yan <32798618+ss100yan@users.noreply.github.com>
2019-02-12 00:48:42 -0500

    new

---
+++ assets/javascript/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $("#container2").hide();
+    $("#container3").hide();
+       
+    
+    $("#Portfolio").click(function(){
+    $("#container").hide();
+    $("#container2").hide();
+    $("#container3").show();
+  });
+  
+  $("#About").click(function(){
+    $("#container").show();
+    $("#container2").hide();
+    $("#container3").hide();
+  });
+  $("#Contact").click(function(){
+    $("#container2").show();
+    $("#container").hide();
+    $("#container3").hide();
+  });
+});
